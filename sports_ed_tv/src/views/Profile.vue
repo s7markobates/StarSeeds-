7f67d63132238b775726e731803eb9b23d023224
s7markobates <marko.bates@s7codedesign.com>
2023-07-07 00:31:56 +0200

    v4

--- sports_ed_tv/src/views/Profile.vue
+++ sports_ed_tv/src/views/Profile.vue
@@ -6,15 +6,15 @@
         <span class="ml-2 hover:text-orange-400">{{ profile.name }} profile</span>
       </div>
     </div>
-    <div class="w-[95%] mx-auto my-3 bg-gray-200 px-4 py-3 rounded-lg shadow-md">
+    <div class="w-[50%] mx-auto my-8 bg-gray-200 px-4 py-3 rounded-lg shadow-md">
       <div class="flex justify-between items-end">
         <div>
           <h1><span class="font-bold">Name: </span><span class="font-semibold">{{ profile.name }}</span></h1>
           <p><span class="font-bold">Email: </span>{{ profile.email }}</p>
         </div>           
-        <img :src="profile.image" class="h-32 w-32 rounded-full shadow-2xl border-2 border-gray-300 mx-5 -mt-16" alt="Profile Image" />
+        <img :src="profile.image" class="h-40 w-40 rounded-full shadow-2xl border-2 border-gray-300 mx-5 -mt-16" alt="Profile Image" />
       </div>
-      <p class="text-justify mt-3">
+      <p class="text-justify mt-7">
         <span class="font-bold">Description: </span>{{ profile.description }}
       </p>
     </div>
