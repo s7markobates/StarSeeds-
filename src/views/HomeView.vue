e77f26fe3a52843e88727cd96cc5506ef85e22ed
s7markobates <marko.bates@s7codedesign.com>
2023-07-15 00:20:36 +0200

    drugi komit, nadam se da ide na granu.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-blue-500 mt-[96px] bg-white h-[600px]">
-    <div class="text-3xl font-bold flex justify-center items-center ">
-      <h1 class="mt-12">Home page</h1>
-      <h1 class="mt-4">Light theme</h1>
+    <div class="text-3xl font-bold flex flex-col justify-center items-center ">
+      <h1 class="mt-2">Home page</h1>
+      <h1 class="mt-2">Light theme</h1>
     </div>
   </div>
 </template>
