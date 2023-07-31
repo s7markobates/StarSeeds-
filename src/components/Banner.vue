e0109d339ef874db377fc1a0fe53430d6866fe15
s7markobates <marko.bates@s7codedesign.com>
2023-07-31 20:03:59 +0200

    Updated a banner component.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="h-[300px]  light border-b-2 border-orange-400 rounded-md">
     <div class="flex flex-col justify-center items-center py-20">
-      <div class="h-[300px] flex justify-center items-center light border-b-2 border-orange-400 rounded-md">
-        <div>
-          <h1 class="text-center text-white text-lg">Join the</h1>
-          <div class="text-[30px] my-auto sm:my-1 sm:text-4xl font-custom">
-            <span class="text-orange-400 mr-[3px]">Star</span>
-            <i class="fas fa-meteor text-orange-400"></i>
-            <span class="text-yellow-400">seeds</span>
-          </div>
-          <h1 class="text-center text-white text-lg">community</h1>
+      <div>
+        <h1 class="text-center text-white text-lg">Join the</h1>
+        <div class="text-[30px] my-auto sm:my-1 sm:text-4xl font-custom">
+          <span class="text-orange-400 mr-[3px]">Star</span>
+          <i class="fas fa-meteor text-orange-400"></i>
+          <span class="text-yellow-400">seeds</span>
         </div>
-        <div>
-          <i class="fas fa-rocket text-orange-400"></i>
-          <i class="fas fa-rocket text-orange-400"></i>
-          <i class="fas fa-rocket text-orange-400"></i>
-          <i class="fas fa-rocket text-orange-400"></i>
-          <i class="fas fa-rocket text-orange-400"></i>
-        </div>
+        <h1 class="text-center text-white text-lg">community</h1>
+      </div>
+      <div>
+        <i class="fas fa-rocket text-orange-400"></i>
+        <i class="fas fa-rocket text-orange-400"></i>
+        <i class="fas fa-rocket text-orange-400"></i>
+        <i class="fas fa-rocket text-orange-400"></i>
+        <i class="fas fa-rocket text-orange-400"></i>
       </div>
     </div>
   </div>      
