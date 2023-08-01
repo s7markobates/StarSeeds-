ddcadf9293cd5edf3b989bd5b4c7154c66d5cfd9
s7markobates <marko.bates@s7codedesign.com>
2023-08-01 23:31:16 +0200

    Slider component is more stylish now.

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,43 +1,41 @@
 <template>
-    <div class="light relative">
-
-        <div v-if="currentSlideIndex === 0" class="grid grid-cols-2 h-[660px] my-4">
-            <div class="flex flex-col justify-center items-end my-auto">
-              <h1 :class="{ 'opacity-0 translate-x-[-30px]': !slideVisible }" class="title transition-all">{{ slideVisible ? 'Private Messaging Redefined' : '' }}</h1>
-              <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all">{{ slideVisible ? 'Welcome to StarSeeds application, the ultimate private messaging app designed with cutting-edge technology and security in mind. Experience seamless, reliable, and encrypted communication like never before. Connect with friends, family, and colleagues across the globe, knowing your conversations are safe from prying eyes. Join the StarSeeds community today and embrace the future of private messaging.' : '' }}</p>
-              <i class="fas fa-meteor text-orange-400 absolute top-3 left-5 opacity-50 text-[250px]"></i>
-            </div>
-            <div class="flex justify-center my-auto ml-16">
-              <img src="../assets/01Chat.png" class="phone-img" alt="">
-              <i class="fas fa-meteor text-orange-400 absolute bottom-3 right-5 opacity-50 text-[470px]"></i>
-            </div>
-        </div>
-
-      <div v-if="currentSlideIndex === 1" class="grid grid-cols-2 h-[660px] my-4">
-        <div class="flex flex-col justify-center items-end my-auto">
-          <h1 :class="{ 'opacity-0 translate-x-[-30px]': !slideVisible }" class="title transition-all">{{ slideVisible ? 'Unparalleled Security' : '' }}</h1>
-          <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all">{{ slideVisible ? 'At StarSeeds, your privacy is our top priority. With end-to-end encryption, your messages are shielded from any unauthorized access, ensuring that only you and the intended recipient can read them. Say goodbye to worries about data breaches or unwanted surveillance. Feel confident in sharing your most personal thoughts and moments, knowing they remain between you and your chosen contacts.' : '' }}</p>
-          <i class="fas fa-meteor text-orange-400 absolute top-3 left-5 opacity-50 text-[250px]"></i>
-        </div>
-        <div class="flex justify-center my-auto ml-16">
-          <img src="../assets/02Profile.png" class="phone-img" alt="">
-          <i class="fas fa-meteor text-orange-400 absolute bottom-3 right-5 opacity-50 text-[470px]"></i>
-        </div>
+  <div class="light relative rounded-lg">
+    <div v-if="currentSlideIndex === 0" class="grid grid-cols-2 h-[660px] my-4">
+      <div class="flex flex-col justify-center items-end my-auto">
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Private Messaging Redefined' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? 'Welcome to StarSeeds application, the ultimate private messaging app designed with cutting-edge technology and security in mind. Experience seamless, reliable, and encrypted communication like never before. Connect with friends, family, and colleagues across the globe, knowing your conversations are safe from prying eyes. Join the StarSeeds community today and embrace the future of private messaging.' : '' }}</p>
+        <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
       </div>
-
-      <div v-if="currentSlideIndex === 2" class="grid grid-cols-2 h-[660px] my-4">
-        <div class="flex flex-col justify-center items-end my-auto">
-          <h1 :class="{ 'opacity-0 translate-x-[-30px]': !slideVisible }" class="title transition-all">{{ slideVisible ? 'Seamless User Experience' : '' }}</h1>
-          <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all">{{ slideVisible ? "StarSeeds delivers a user-friendly interface that makes messaging effortless and enjoyable. From intuitive navigation to customizable themes, we provide the tools to create your perfect messaging environment. Whether it's one-on-one chats or group conversations, our app offers a seamless user experience that fosters stronger connections and brings people closer together. Try StarSeeds now and elevate your messaging game!" : "" }}</p>
-          <i class="fas fa-meteor text-orange-400 absolute top-3 left-5 opacity-50 text-[250px]"></i>
-        </div>
-        <div class="flex justify-center my-auto ml-16">
-          <img src="../assets/03Messages.png" class="phone-img" alt="">
-          <i class="fas fa-meteor text-orange-400 absolute bottom-3 right-5 opacity-50 text-[470px]"></i>
-        </div>
+      <div class="flex justify-center my-auto ml-16">
+        <img src="../assets/01Chat.png" class="phone-img" alt="">
+        <i class="fas fa-meteor text-orange-400 absolute bottom-8 right-12 opacity-60 text-[470px]"></i>
       </div>
-
     </div>
+
+    <div v-if="currentSlideIndex === 1" class="grid grid-cols-2 h-[660px] my-4">
+      <div class="flex flex-col justify-center items-end my-auto">
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Unparalleled Security' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? 'At StarSeeds, your privacy is our top priority. With end-to-end encryption, your messages are shielded from any unauthorized access, ensuring that only you and the intended recipient can read them. Say goodbye to worries about data breaches or unwanted surveillance. Feel confident in sharing your most personal thoughts and moments, knowing they remain between you and your chosen contacts.' : '' }}</p>
+        <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
+      </div>
+      <div class="flex justify-center my-auto ml-16">
+        <img src="../assets/02Profile.png" class="phone-img" alt="">
+        <i class="fas fa-meteor text-orange-400 absolute bottom-8 right-12 opacity-60 text-[470px]"></i>
+      </div>
+    </div>
+
+    <div v-if="currentSlideIndex === 2" class="grid grid-cols-2 h-[660px] my-4">
+      <div class="flex flex-col justify-center items-end my-auto">
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Seamless User Experience' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? "StarSeeds delivers a user-friendly interface that makes messaging effortless and enjoyable. From intuitive navigation to customizable themes, we provide the tools to create your perfect messaging environment. Whether it's one-on-one chats or group conversations, our app offers a seamless user experience that fosters stronger connections and brings people closer together. Try StarSeeds now and elevate your messaging game!" : "" }}</p>
+        <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
+      </div>
+      <div class="flex justify-center my-auto ml-16">
+        <img src="../assets/03Messages.png" class="phone-img" alt="">
+        <i class="fas fa-meteor text-orange-400 absolute bottom-8 right-12 opacity-60 text-[470px]"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -61,13 +59,13 @@ onMounted(() => {
 
 <style scoped>
 .title {
-  @apply text-3xl text-yellow-400 text-end
+  @apply text-3xl text-yellow-400 text-end font-custom
 }
 .paragraph {
-  @apply pl-28 py-3 sm:text-sm text-white text-justify indent-12
+  @apply pl-28 py-3 sm:text-base text-white text-justify indent-12 z-[1]
 }
 .phone-img {
-    @apply h-[550px] rounded-[44px] z-[1] shadow-xl shadow-orange-400
+    @apply h-[550px] rounded-[44px] z-[1] shadow-lg shadow-orange-400
 }
 
 .animate-slide-left {
@@ -84,5 +82,8 @@ onMounted(() => {
     opacity: 1;
     transform: translateX(0);
   }
+}
+.font-custom {
+  font-family: 'Yatra One', cursive;
 }
 </style>
