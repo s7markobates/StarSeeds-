db1f19bc762a4ef9162ec5a31e97a92d8ee09c93
s7markobates <marko.bates@s7codedesign.com>
2023-08-02 10:45:44 +0200

    Duration of slider text is a bit slower now.

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,8 +2,8 @@
   <div class="light relative rounded-lg">
     <div v-if="currentSlideIndex === 0" class="grid grid-cols-2 h-[660px] my-4">
       <div class="flex flex-col justify-center items-end my-auto">
-        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Private Messaging Redefined' : '' }}</h1>
-        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? 'Welcome to StarSeeds application, the ultimate private messaging app designed with cutting-edge technology and security in mind. Experience seamless, reliable, and encrypted communication like never before. Connect with friends, family, and colleagues across the globe, knowing your conversations are safe from prying eyes. Join the StarSeeds community today and embrace the future of private messaging.' : '' }}</p>
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-500">{{ slideVisible ? 'Private Messaging Redefined' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-500">{{ slideVisible ? 'Welcome to StarSeeds application, the ultimate private messaging app designed with cutting-edge technology and security in mind. Experience seamless, reliable, and encrypted communication like never before. Connect with friends, family, and colleagues across the globe, knowing your conversations are safe from prying eyes. Join the StarSeeds community today and embrace the future of private messaging.' : '' }}</p>
         <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
       </div>
       <div class="flex justify-center my-auto ml-16">
@@ -14,8 +14,8 @@
 
     <div v-if="currentSlideIndex === 1" class="grid grid-cols-2 h-[660px] my-4">
       <div class="flex flex-col justify-center items-end my-auto">
-        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Unparalleled Security' : '' }}</h1>
-        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? 'At StarSeeds, your privacy is our top priority. With end-to-end encryption, your messages are shielded from any unauthorized access, ensuring that only you and the intended recipient can read them. Say goodbye to worries about data breaches or unwanted surveillance. Feel confident in sharing your most personal thoughts and moments, knowing they remain between you and your chosen contacts.' : '' }}</p>
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-500">{{ slideVisible ? 'Unparalleled Security' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-500">{{ slideVisible ? 'At StarSeeds, your privacy is our top priority. With end-to-end encryption, your messages are shielded from any unauthorized access, ensuring that only you and the intended recipient can read them. Say goodbye to worries about data breaches or unwanted surveillance. Feel confident in sharing your most personal thoughts and moments, knowing they remain between you and your chosen contacts.' : '' }}</p>
         <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
       </div>
       <div class="flex justify-center my-auto ml-16">
@@ -26,8 +26,8 @@
 
     <div v-if="currentSlideIndex === 2" class="grid grid-cols-2 h-[660px] my-4">
       <div class="flex flex-col justify-center items-end my-auto">
-        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-1000">{{ slideVisible ? 'Seamless User Experience' : '' }}</h1>
-        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-1000">{{ slideVisible ? "StarSeeds delivers a user-friendly interface that makes messaging effortless and enjoyable. From intuitive navigation to customizable themes, we provide the tools to create your perfect messaging environment. Whether it's one-on-one chats or group conversations, our app offers a seamless user experience that fosters stronger connections and brings people closer together. Try StarSeeds now and elevate your messaging game!" : "" }}</p>
+        <h1 :class="{ 'opacity-0 translate-x-[-50px]': !slideVisible }" class="title transition-all duration-500">{{ slideVisible ? 'Seamless User Experience' : '' }}</h1>
+        <p :class="{ 'opacity-0 translate-x-[-20px]': !slideVisible }" class="paragraph transition-all duration-500">{{ slideVisible ? "StarSeeds delivers a user-friendly interface that makes messaging effortless and enjoyable. From intuitive navigation to customizable themes, we provide the tools to create your perfect messaging environment. Whether it's one-on-one chats or group conversations, our app offers a seamless user experience that fosters stronger connections and brings people closer together. Try StarSeeds now and elevate your messaging game!" : "" }}</p>
         <i class="fas fa-meteor text-orange-400 absolute top-20 left-12 opacity-60 text-[250px]"></i>
       </div>
       <div class="flex justify-center my-auto ml-16">
