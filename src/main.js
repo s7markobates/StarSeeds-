8fe21dd97f3790c4059ddef08e5ecfaa04af82f1
s7markobates <marko.bates@s7codedesign.com>
2023-08-03 13:26:07 +0200

    Added intersec on main.js.

--- src/main.js
+++ src/main.js
@@ -7,4 +7,23 @@ const app = createApp(App)
 
 app.use(router)
 
+app.directive('intersect', {
+    beforeMount(el, binding) {
+        const options = {
+            root: null, // koristiti viewport kao root
+            rootMargin: '0px', // margin root elementa
+            threshold: 0 // prag vidljivosti elemenata
+        }
+  
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+            binding.value(entries)
+            observer.disconnect()
+            }
+        }, options)
+    
+        observer.observe(el)
+    }
+})
+
 app.mount('#app')
