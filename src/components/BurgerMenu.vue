e7c1312f5089ed161db1f0774f9091a602e25da8
s7markobates <marko.bates@s7codedesign.com>
2023-12-29 00:03:24 +0100

    cosmetic changes.

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -69,7 +69,7 @@ export default {
       activeIndex: null,
       items: [
         {
-          title: 'About bts - Author of StarSeeds App',
+          title: 'About bts - Developer of the StarSeeds App',
           content:
             'In progress Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
